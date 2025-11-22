7fbb27311356f02345fb002cdfca83181b7303b2
Sameer Sapkota <acidicsameer.namrmc008@gmail.com>
2025-11-22 21:07:59 +0545

    day 1 of mern 1.0

---
+++ app.js
@@ -0,0 +1,14 @@
+ const a=6 
+ const b=6 
+ console.log(a+b) 
+  const arr=[1,2,4,5,6,7] 
+  console.log(arr) 
+   const person ={
+ name:"samir",
+ Age:12, 
+ hobby:"crickter",
+   } 
+   console.log(person) 
+   console.log(person.name)
+   console.log(person.Age)
+   console.log(person.hobby)
