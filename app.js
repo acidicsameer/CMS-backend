b8f6d695b41bf22a6f4fff4cd3c27efc69d88869
Sameer Sapkota <acidicsameer.namrmc008@gmail.com>
2025-11-23 21:59:56 +0545

    day 8 completed

--- app.js
+++ app.js
@@ -8,42 +8,74 @@ import Blog from "./model/blog.model.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
- app.use(express.json()) 
- app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: " hello baedhjd ",
+//     name: "samir",
+//     yetikai: "sagar",
+//     status: 200,
+//   });
+// });
+
+//create blog api
+ 
+app.post("/blog", async (req, res) => {
+  const {title,subTitle,description}=req.body
+
+  try {
+    const data = await Blog.create({
+      title,
+      subTitle,
+      description,
+    });
+
+    res.json({
+      message: "successfully added data to the database",
+      data: data,
+    });
+  } catch (error) {
+    console.log("error occured while adding data to database ", error);
+  }
+});
+
+// get all blogs
+app.get("/blog", async(req,res) => { 
+  const data =await Blog.find()
+   
   res.json({
-    message: " hello baedhjd ",
-    name: "samir",
-    yetikai: "sagar",
-    status: 200,
+    message:" successfully  get all blogs ",
+    data:data,
+    
   });
 });
 
+ // get single  blog 
 
-//create blog api
+ app.get("/blog/:id", async(req,res)=>{  
+   
+ const id =req.params.id
 
-app.post("/addBlog", async(req,res) => { 
-
-  try {
-     
-      const data=await Blog.create({ 
-      title:req.body.title,
-      subTitle:req.body.subTitle, 
-      description:req.body.description,
-    })  
-
+  const data = await  Blog.findById(id)  
+   if(data)
+   {
     res.json({
-       "message":"successfully added data to the database", 
-       data:data
+     "message":" error occured while getting a single data " ,
+    
+  })
+   }
 
-    })
+ else {
 
-  } catch (error) {
-    console.log("error occured while adding data to database ",error)
-  } 
+  res.json({
+     "message":" get a single blog successfully" ,
+     data:data,
+  })
+}
+ })
 
-});
 
 app.listen(port, () => {
   dbConnect();
